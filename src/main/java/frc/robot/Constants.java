e0164a92bb2f3ab9694b916ac13bdbd4b6f5ba5f
Eclipse-5214 <elijahlipscomb55@gmail.com>
2025-03-05 14:54:31 -0500

    change speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,9 +20,9 @@ public final class Constants {
     public static final double  DEADBAND           = 0.07;
 
     // Scales for movement and rotation (1 is full speed)
-    public static final double  TRANSLATION_SCALE  = 0.8;
-    public static final double  ROTATION_SCALE     = 0.8;
+    public static final double  TRANSLATION_SCALE  = 0.6;
+    public static final double  ROTATION_SCALE     = 0.6;
   }
 
-  public static final   double  MAX_SPEED = 4.5;
+  public static final   double  MAX_SPEED = 2.5;
 }
